9d0c06e262ba7ee488d9397ecbdf294a36dc524f
pramirez4 <59981840+pramirez4@users.noreply.github.com>
2020-01-23 16:59:05 -0600

    Dew point
    Weather formula started

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -6,17 +6,17 @@ namespace BasicCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** BASIC CALCULATOR ***");
+            Console.WriteLine("*** BASIC Wether CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
+            Console.WriteLine("Enter Temperature in Fehrenheit: ");
 
             // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
+            int temperature = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Relative Humidity: ");
+            int relativeHumidity = int.Parse(Console.ReadLine());
 
-            int sum = firstNumber + secondNumber;
+            int sum = temperature - 9 * (100 - relativeHumidity) / 25 ;
 
             Console.WriteLine("The answer is " + sum);
         }
